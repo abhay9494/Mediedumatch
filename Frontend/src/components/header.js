78a791579109c2630ac6e27c43c7c49329b852a8
abhay9494 <abhayprasad9494@gmail.com>
2024-05-24 23:57:00 +0530

    Backend Updated

--- Frontend/src/components/header.js
+++ Frontend/src/components/header.js
@@ -24,18 +24,19 @@ function Header() {
   return (
     <Navbar bg="light" expand="lg" className="d-flex justify-content-between">
       <Container className="d-flex justify-content-between">
-        <Navbar.Brand href="#home">Mediedumatch</Navbar.Brand>
+        <Navbar.Brand href="/">Mediedumatch</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse
           id="basic-navbar-nav"
           className="d-flex justify-content-between w-100"
         >
           <Nav className="mx-auto">
-            <Nav.Link id="home" style={{ marginLeft: "30px" }} href="/">
+            <Nav.Link id="home" className="links" style={{ marginLeft: "30px" }} href="/">
               Home
             </Nav.Link>
             <Nav.Link
               id="collegepredictor"
+              className="links"
               style={{ marginLeft: "30px" }}
               href="/collegepredictor"
             >
@@ -43,16 +44,18 @@ function Header() {
             </Nav.Link>
             <Nav.Link
               id="rankpredictor"
+              className="links"
               style={{ marginLeft: "30px" }}
               href="/rankpredictor"
             >
               Rank Predictor
             </Nav.Link>
-            <Nav.Link id="team" style={{ marginLeft: "30px" }} href="/teams">
+            <Nav.Link id="team" className="links" style={{ marginLeft: "30px" }} href="/teams">
               Teams
             </Nav.Link>
             <Nav.Link
               id="contact"
+              className="links"
               style={{ marginLeft: "30px" }}
               href="/contact"
             >
